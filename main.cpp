7a5c38383bd7afb61056d0dc2b7e50d1d3547cd6
hfondecave <63591601+hfondecave@users.noreply.github.com>
2021-02-18 09:35:59 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -14,36 +14,36 @@ int main()
 
   for(int i = 0; i < Nb_part; i++){
     //Génération des conditions initiales
-    double X1=(rand()%100)/100;
+    double X1=rand()/(double)RAND_MAX;
     class Particule P;
-    double r=pow(0.999*pow(X1,-2/3)-1,-1/2);
-    double X2=(rand()%100)/100;
-    double X3=(rand()%100)/100;
+    double r=pow(0.999*pow(X1,-2.0/3.0)-1,-1.0/2.0);
+    double X2=rand()/(double)RAND_MAX;
+    double X3=rand()/(double)RAND_MAX;
     double u=sqrt(pow(X2,2)+pow(X3,2));
     while (u>1){
-        X2=(rand()%100)/100;
-        X3=(rand()%100)/100;
+        X2=rand()/(double)RAND_MAX;
+        X3=rand()/(double)RAND_MAX;
         u=sqrt(pow(X2,2)+pow(X3,2));
     }
     P.x=r*X2/u;
     P.y=r*X3/u;
     //Méthode de rejet pour déterminer q
-    double X5=(rand()%100)/100;
-    double X6=(rand()%100)/100;
-    double g=pow(X5,2)*pow(1-pow(X5,2),7/2);
+    double X5=rand()/(double)RAND_MAX;
+    double X6=rand()/(double)RAND_MAX;
+    double g=pow(X5,2)*pow(1-pow(X5,2),7.0/2.0);
     while (X6>10*g){
-        X5=(rand()%100)/100;
-        X6=(rand()%100)/100;
-        g=pow(X5,2)*pow(1-pow(X5,2),7/2);
+        X5=rand()/(double)RAND_MAX;
+        X6=rand()/(double)RAND_MAX;
+        g=pow(X5,2)*pow(1-pow(X5,2),7.0/2.0);
     }
     double q=X5;
     double v=vitesse_echappement(P,q);
-    double X7=(rand()%100)/100;
-    double X8=(rand()%100)/100;
+    double X7=rand()/(double)RAND_MAX;
+    double X8=rand()/(double)RAND_MAX;
     double uv=sqrt(pow(X7,2)+pow(X8,2));
     while (uv>1){
-        X7=(rand()%100)/100;
-        X8=(rand()%100)/100;
+        X7=rand()/(double)RAND_MAX;
+        X8=rand()/(double)RAND_MAX;
         uv=sqrt(pow(X7,2)+pow(X8,2));
     }
     P.vx=v*X7/uv;
